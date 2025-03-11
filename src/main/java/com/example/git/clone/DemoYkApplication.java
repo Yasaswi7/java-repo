b52623f78a9c92cafe17d80f7191315cef207d04
yasaswi7 <yasaswi2398@gmail.com>
2025-03-11 15:25:15 -0400

    Push test 2

--- src/main/java/com/example/git/clone/DemoYkApplication.java
+++ src/main/java/com/example/git/clone/DemoYkApplication.java
@@ -1,6 +1,5 @@
-package com.example.demo_yk;
+package com.example.git.clone;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
--- src/test/java/com/example/git/clone/DemoYkApplicationTests.java
+++ src/test/java/com/example/git/clone/DemoYkApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo_yk;
+package com.example.git.clone;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
