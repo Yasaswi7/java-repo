f2ae6b0e0a228fbd4cc38be807c820ca2d48ec4c
Yasaswi7 <45798041+Yasaswi7@users.noreply.github.com>
2025-03-10 22:33:08 -0400

    Add files via upload

---
+++ src/main/java/com/example/demo_yk/DemoYkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_yk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoYkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoYkApplication.class, args);
+	}
+
+}
