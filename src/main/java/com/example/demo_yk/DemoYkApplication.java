85595110e916de54b26f83a2b89391b217d5919e
yasaswi7 <yasaswi2398@gmail.com>
2025-03-10 22:45:29 -0400

    Push-test1

--- src/main/java/com/example/demo_yk/DemoYkApplication.java
+++ src/main/java/com/example/demo_yk/DemoYkApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoYkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoYkApplication.class, args);
+
+		//SpringApplication.run(DemoYkApplication.class, args);
+		System.out.println("Push-test");
 	}
 
 }
